cb0f18b830d1fedf8ac9e07fbce574c806bc68d8
Daire Ni Chathain <dairenichat@gmail.com>
2019-03-09 16:36:28 +0000

    added multiple pins to game

---
+++ BowlingGameGUI/Assets/PinController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+    // constant for pin name
+    private const string PIN = "Pin";
+
+    // List of bowling pins
+    [SerializeField]
+    private IList<GameObject>pins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get all the bowling pins & add to list
+        this.pins = GameObject.FindGameObjectsWithTag(PIN);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*     foreach (GameObject p in this.pins)
+              {
+
+                  Debug.Log("");
+              }
+          }
+          */
+    }
+}
---
+++ BowlingGameGUI/Assets/_Scripts/Pin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (gameObject.transform.up.y < 70)
+        {
+          // Debug.Log("pin fell");
+        }
+
+
+
+        if (transform.up.y < 0.0)
+        {
+            // Fallen 1.0 (upright) and 0.0 (flat on ground).
+            Debug.Log("pin fell");
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Plane")
+        {
+          //S  Debug.Log("pin fell");
+        }
+    }
+}
